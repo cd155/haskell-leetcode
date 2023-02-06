d9b6a05df2ab248fed4041df2b466c329a90d32b
cd155 <github@postmail.app>
2023-02-05 21:04:57 -0500

    add two sum

---
+++ main/WeekOne.hs
@@ -0,0 +1,14 @@
+{-
+    Two Sum:
+    
+    Given an array of integers and an integer target, 
+    return indexes of the two numbers that they add up to target.
+
+    You may assume that each input would have exactly one solution, 
+    and you may not use the same element twice.
+
+    You can return the answer in any order.
+-}
+
+twoSum :: [Integer] -> (Integer, Integer)
+twoSum xs = error "Not Implement"
