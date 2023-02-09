8e41a1be9283e79efad35355a18518355a1bec53
cd155 <github@postmail.app>
2023-02-09 13:57:45 -0500

    refactor test cases

--- main/TestCases.hs
+++ main/TestCases.hs
@@ -1,17 +1,15 @@
 import WeekOne
 
 -- Two sum
-sampleTS = 
+sampleTwoSum = 
     [
-        [2,7,11,15],
-        [3,2,4],
-        [3,3],
-        [5,2,3,5,1,7,1]
+        ([2,7,11,15], 9),
+        ([3,2,4], 6),
+        ([3,3], 6),
+        ([5,2,3,5,1,7,1],2)
     ]
 
-targetTS = [9,6,6,2]
-
-answerTS = 
+answerTwoSum = 
     [
         (0,1),
         (1,2),
@@ -19,19 +17,18 @@ answerTS =
         (4,6)
     ]
 
-runTwoSumTest :: [[Integer]] -> [Integer]-> [(Nat,Nat)] -> Bool
-runTwoSumTest [] _ _ = True
-runTwoSumTest _ [] _ = True
-runTwoSumTest _ _ [] = True
-runTwoSumTest (x:xs) (y:ys) (z:zs)
-    | (twoSumBF x y) == z && (twoSumHash x y) == z = 
-        True && runTwoSumTest xs ys zs
+runTwoSumTest :: [([Integer],Integer)]-> [(Nat,Nat)] -> Bool
+runTwoSumTest [] _ = True
+runTwoSumTest _ [] = True
+runTwoSumTest (x:xs) (y:ys)
+    | (twoSumBF (fst x) (snd x)) == y && (twoSumHash (fst x) (snd x)) == y = 
+        True && runTwoSumTest xs ys
     | otherwise = error (
-        "Input: " ++ show (x,y) ++ 
-        "is not match with Output: "++ show z)
+        "Input: " ++ show (fst x,snd x) ++ 
+        "is not match with Output: "++ show y)
 
-sampleIVP = ["()", "()[]{}", "([{()}{}])", "(]", "(([][])[})"]
-answerIVP = [True, True, True, False, False]
+sampleIsValidParen = ["()", "()[]{}", "([{()}{}])", "(]", "(([][])[})"]
+answerIsValidParen = [True, True, True, False, False]
 
 runIsValidParen :: [String] -> [Bool] -> Bool
 runIsValidParen [] _ = True
@@ -43,8 +40,8 @@ runIsValidParen (x:xs) (y:ys)
         "is not match with Output: "++ show y)
 
 testAll = do
-    let ts = runTwoSumTest sampleTS targetTS answerTS
-    let ivp = runIsValidParen sampleIVP answerIVP
+    let ts = runTwoSumTest sampleTwoSum answerTwoSum
+    let ivp = runIsValidParen sampleIsValidParen answerIsValidParen
 
     if ts && ivp then
         putStrLn "All cases passed."
