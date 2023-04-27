6ed5ce447906298b21e7677540f5056da6f4a98a
cd155 <cd155@163.com>
2023-04-27 12:56:26 -0400

    add Partition Equal Subset Sum

--- main/WeekSix.hs
+++ main/WeekSix.hs
@@ -26,3 +26,56 @@ wordBreakAux [] word _ = if null word then True else False
 wordBreakAux (x:xs) word dict
   | (word++[x]) `elem` dict = wordBreakAux xs [] dict
   | otherwise = wordBreakAux xs (word++[x]) dict
+
+{-
+  51. Partition Equal Subset Sum
+
+  Given an [Int], return true if you can partition the array into two 
+  subsets such that the sum of the elements in both subsets is equal or 
+  otherwise false
+
+  Test Cases:
+  partEqualSetSum [1,11 5]    -> 
+    [[1],[11,1],[5,1],[5,11,1],[11],[5,11],[5]]
+  partEqualSetSum [1,2,3,4]   -> 
+  
+  partEqualSetSum' [1,5,11,5] -> True
+  partEqualSetSum' [1,2,3,5]  -> False
+-}
+
+-- return all possible subsets
+partEqualSetSum :: [Int] -> [[Int]] 
+partEqualSetSum [] = []
+partEqualSetSum (x:xs) = 
+  partEqualSetSumAux [([x],0)] xs ++ partEqualSetSum xs
+
+{-
+  partEqualSetSumAux [([1],0)] [2,3,4]
+
+  cur: current list
+  i: the skip index, once one element be used, it will become unavailable
+  ys: available elements 
+-}
+partEqualSetSumAux :: [([Int],Int)] -> [Int] -> [[Int]]
+partEqualSetSumAux [] _ = []
+partEqualSetSumAux ((cur,i):xs) ys = 
+  cur: (partEqualSetSumAux (xs ++ allComb) ys)
+  where allComb = zip (map (\y -> y:cur) (drop i ys)) [(i+1)..length ys]
+
+
+partEqualSetSum' :: [Int] -> Bool
+partEqualSetSum' xs 
+  | odd sumXs = False
+  | otherwise = partEqualSetSumLoop' xs (sumXs `div` 2)
+  where sumXs = sum xs
+
+partEqualSetSumLoop' :: [Int] -> Int -> Bool
+partEqualSetSumLoop' [] target = False
+partEqualSetSumLoop' (x:xs) target = 
+  partEqualSetSumAux' [([x],0)] xs target || partEqualSetSumLoop' xs target
+
+partEqualSetSumAux' :: [([Int],Int)] -> [Int] -> Int -> Bool
+partEqualSetSumAux' [] _ _ = False
+partEqualSetSumAux' ((cur,i):xs) ys target = 
+  if sum cur == target then True else partEqualSetSumAux' (xs ++ allComb) ys target
+  where allComb = zip (map (\y -> y:cur) (drop i ys)) [(i+1)..length ys]
